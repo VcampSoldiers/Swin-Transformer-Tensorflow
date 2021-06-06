d614c8440afafd11766402b051a0e9a414275dc0
DHdroid <happydh1@snu.ac.kr>
2021-06-06 16:06:58 +0900

    Add WindowAttention Layer

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import (
     Dense,
     Dropout,
+    Softmax
 )
 from tensorflow.keras import Model, Layer, Sequential
 
@@ -59,3 +60,96 @@ def window_reverse(windows, window_size, H, W):
     x = tf.reshape(windows, [B, H // window_size, W // window_size, window_size, window_size, -1])
     x = tf.reshape(tf.transpose(x, perm=[0, 1, 3, 2, 4, 5]), [B, H, W, -1])
     return x
+
+
+class WindowAttention(Layer):
+    r""" Window based multi-head self attention (W-MSA) module with relative position bias.
+    It supports both of shifted and non-shifted window.
+    Args:
+        dim (int): Number of input channels.
+        window_size (tuple[int]): The height and width of the window.
+        num_heads (int): Number of attention heads.
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+        proj_drop (float, optional): Dropout ratio of output. Default: 0.0
+    """
+
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size  # Wh, Ww
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # define a parameter table of relative position bias
+        initializer = tf.keras.initializers.TruncatedNormal(mean=0., stddev=.02)
+        self.relative_position_bias_table = tf.Variable(
+            initializer(shape=(2*self.window_size[0]-1, 2*self.window_size[1]-1, num_heads)), trainable=True)  # 2*Wh-1 * 2*Ww-1, nH
+
+        # get pair-wise relative position index for each token inside the window
+        coords_h = tf.range(self.window_size[0])
+        coords_w = tf.range(self.window_size[1])
+        coords = tf.stack(tf.meshgrid(coords_h, coords_w))  # 2, Wh, Ww
+        coords_flatten = tf.reshape(coords, [2, -1])  # 2, Wh*Ww
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+        relative_coords = tf.transpose(relative_coords, perm=[1,2,0]) # Wh*Ww, Wh*Ww, 2
+        relative_coords = relative_coords + [self.window_size[0] - 1, self.window_size[1] - 1]  # shift to start from 0
+        relative_coords = relative_coords * [2*self.window_size[1] - 1, 1]
+        self.relative_position_index = tf.math.reduce_sum(relative_coords,-1)  # Wh*Ww, Wh*Ww
+
+        self.qkv = Dense(dim * 3, bias=qkv_bias)
+        self.attn_drop = Dropout(attn_drop)
+        self.proj = Dense(dim, dim)
+        self.proj_drop = Dropout(proj_drop)
+        self.softmax = Softmax(axis=-1)
+
+    def call(self, x, mask=None):
+        """
+        Args:
+            x: input features with shape of (num_windows*B, N, C)
+            mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
+        """
+        B_, N, C = x.shape
+        qkv = tf.transpose(tf.reshape(self.qkv(x), [B_, N, 3, self.num_heads, C // self.num_heads]), perm=[2, 0, 3, 1, 4]) # [3, B_, num_head, Ww*Wh, C//num_head]
+        q, k, v = tf.unstack(qkv)  # make torchscript happy (cannot use tensor as tuple)
+
+        q = q * self.scale
+        attn = tf.einsum('...ij,...kj->...ik', q, k)
+
+        relative_position_bias = tf.reshape(self.relative_position_bias_table[tf.reshape(self.relative_position_index, -1)],
+            [self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1])  # Wh*Ww,Wh*Ww,nH
+        relative_position_bias = tf.transpose(relative_position_bias, perm=[2, 0, 1])  # nH, Wh*Ww, Wh*Ww
+        attn = attn + relative_position_bias
+
+        if mask is not None:
+            nW = mask.shape[0] # every window has different mask [nW, N, N]
+            attn = tf.reshape(attn, [B_ // nW, nW, self.num_heads, N, N]) + mask[:, None, :, :] # add mask: make each component -inf or just leave it
+            attn = tf.reshape(attn, [-1, self.num_heads, N, N])
+            attn = self.softmax(attn)
+        else:
+            attn = self.softmax(attn)
+
+        attn = self.attn_drop(attn)
+
+        x = tf.reshape(tf.transpose(attn @ v, perm=[0, 2, 1, 3]), [B_, N, C])
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+    def extra_repr(self) -> str:
+        return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
+
+    def flops(self, N):
+        # calculate flops for 1 window with token length of N
+        flops = 0
+        # qkv = self.qkv(x)
+        flops += N * self.dim * 3 * self.dim
+        # attn = (q @ k.transpose(-2, -1))
+        flops += self.num_heads * N * (self.dim // self.num_heads) * N
+        #  x = (attn @ v)
+        flops += self.num_heads * N * N * (self.dim // self.num_heads)
+        # x = self.proj(x)
+        flops += N * self.dim * self.dim
+        return flops
